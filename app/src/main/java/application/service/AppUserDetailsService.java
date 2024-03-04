ed37b3ae144f5e35a8cfa3475c119288267a798d
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-03-04 17:49:52 +0000

    create login screen authentication

---
+++ app/src/main/java/application/service/AppUserDetailsService.java
@@ -0,0 +1,34 @@
+package application.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import application.repository.UserRepository;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        application.model.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+
+        UserDetails userDetails = 
+            org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                    .roles("USER")
+                    .build();
+                    
+        return userDetails;
+    }
+    
+}
